67fd91b62899a038a6d2512a5264acd85a72d6ef
Kapil Chouhan <kpl.chouhan99@gmail.com>
2020-01-24 19:51:10 +0530

    Final commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'search' => 'search#index'
+  devise_for :users
+  get 'home/index'
+  root to: 'home#index'
+  resources :users, only: [:show, :edit, :update]
+  resources :posts, only: [:new, :create, :show, :destroy]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
